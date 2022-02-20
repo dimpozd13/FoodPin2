b32bee2f2a1fc2c8da06db0dfa53dc30a1375051
Dimpozd <dimpozd@yandex.ru>
2022-02-20 20:06:02 +0300

    test

--- FoodPin/RestaurantListView.swift
+++ FoodPin/RestaurantListView.swift
@@ -127,7 +127,7 @@ struct BasicTextImageRow: View {
                     Image(systemName: "phone")
                 }
             }
-        
+        ///test
             Button(action: {
                 self.restaurant.isFavorite.toggle()
             }) {
